9da179fd007f4a9bb557fb030969e3fed8dafc94
kopher <kopher1601@gmail.com>
2025-03-19 09:46:03 +0900

    Complete first step chat server

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
-package chatserver
+package main
+
+import (
+	"chatserver/network"
+	"log"
+)
 
 func main() {
-
+	server := network.NewServer()
+	log.Fatalln(server.StartServer())
 }
---
+++ network/network.go
@@ -0,0 +1,38 @@
+package network
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+type Network struct {
+	engin *gin.Engine
+}
+
+func NewServer() *Network {
+	n := &Network{
+		engin: gin.New(),
+	}
+	n.engin.Use(gin.Logger())
+	n.engin.Use(gin.Recovery())
+	n.engin.Use(cors.New(cors.Config{
+		AllowWebSockets:  true,
+		AllowCredentials: true,
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"},
+		AllowHeaders:     []string{"*"},
+	}))
+
+	r := NewRoom()
+	go r.RunInit()
+
+	n.engin.GET("/room", r.SocketServe)
+
+	return n
+}
+
+func (n *Network) StartServer() error {
+	log.Println("Starting server...")
+	return n.engin.Run(":8080")
+}
